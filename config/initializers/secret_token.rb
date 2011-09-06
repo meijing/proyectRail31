b13a1c20f3e4d82e6af294463a582953f55020e6
Your Name <you@example.com>
2011-09-06 21:05:43 +0200

    subida primeira generacion de scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProyectoRail31::Application.config.secret_token = 'a503f2b22134675f9af5d9d12e0097273cf956680cb6568c7a7792e568f21c4e58732010e47efd6e7c3f029d5457351e617ba22b9376d6e70d4027a8b3c56267'
